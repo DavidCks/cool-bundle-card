92efbeb6fd3ea1d7966e10a32f284b3a445c9419
David Christ <davidckss@proton.me>
2024-05-29 21:36:47 +0900

    added labeling and label onClick

--- .storybook/Component.stories.jsx
+++ .storybook/Component.stories.jsx
@@ -3,6 +3,12 @@ import { CoolBundleCard, CoolBundleCardElement } from "../index";
 import { placeholder } from "./placeholder.base64.js";
 
 const Component = () => {
+  const [labelClicked, setLabelClicked] = React.useState(false);
+
+  const reset = () => {
+    setLabelClicked(false);
+  };
+
   return (
     <div
       style={{
@@ -13,16 +19,45 @@ const Component = () => {
     >
       <div
         style={{
-          backgroundColor: "#e5e5f7",
+          backgroundColor: "#00000088",
           padding: "20px",
-          opacity: "0.8",
-          backgroundImage:
-            "linear-gradient(135deg, #444cf7 25%, transparent 25%), linear-gradient(225deg, #444cf7 25%, transparent 25%), linear-gradient(45deg, #444cf7 25%, transparent 25%), linear-gradient(315deg, #444cf7 25%, #e5e5f7 25%)",
           backgroundPosition: "10px 0, 10px 0, 0 0, 0 0",
           backgroundSize: "20px 20px",
           backgroundRepeat: "repeat",
         }}
       >
+        <CoolBundleCard
+          title="Cool Bundle Card"
+          label="label"
+          labelMuted={labelClicked}
+          onLabelClick={() => {
+            setLabelClicked(!labelClicked);
+          }}
+        >
+          <CoolBundleCardElement
+            selected={true}
+            name="Placeholder 1"
+            image={placeholder}
+          ></CoolBundleCardElement>
+          <CoolBundleCardElement
+            name="Placeholder 2"
+            image={placeholder}
+          ></CoolBundleCardElement>
+          <CoolBundleCardElement
+            name="Placeholder 3"
+            image={placeholder}
+          ></CoolBundleCardElement>
+          <CoolBundleCardElement
+            name="Placeholder 4"
+            image={placeholder}
+          ></CoolBundleCardElement>
+          <CoolBundleCardElement
+            selected={true}
+            name="Placeholder 5"
+            image={placeholder}
+          ></CoolBundleCardElement>
+        </CoolBundleCard>
+        <div style={{ height: "40px" }}></div>
         <CoolBundleCard title="Cool Bundle Card">
           <CoolBundleCardElement
             selected={true}
@@ -48,6 +83,7 @@ const Component = () => {
           ></CoolBundleCardElement>
         </CoolBundleCard>
       </div>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 };
--- src/CoolBundleCard.jsx
+++ src/CoolBundleCard.jsx
@@ -8,16 +8,34 @@ import React from "react";
  * @param {Object} props The properties passed to the component.
  * @param {string | React.ReactNode} props.title The title content to display at the bottom of the container.
  * @param {string | React.ReactNode} [props.subtitle=""] The subtitle content to display next to the title in a smaller font.
- * @param {string} [props.radius="20px"] The border radius of the container.
+ * @param {string | React.ReactNode} [props.label=""] The label displayed at the bottom of the container
+ * @param {string | undefined} [props.labelBackground="linear-gradient(0.25turn, rgb(255, 104, 240), rgb(91, 196, 255))" | "#00000033"] The value of the css background property for the label.
+ * @param {VoidFunction} [props.onLabelClick] The function to call when the label is clicked.
+ * @param {bool} [props.labelMuted=false] Whether the label and its interactions should be muted
+ * @param {string} [props.radius="40px"] The border radius of the container.
  * @param {React.ReactNode} props.children The child components to be displayed within the container.
  * @returns {JSX.Element} The JSX code for rendering the component.
  */
 export const CoolBundleCard = ({
   title,
   subtitle = "",
-  radius = "20px",
+  label = "",
+  labelBackground,
+  labelMuted = false,
+  onLabelClick,
+  radius = "40px",
   children,
 }) => {
+  const [isLabelHovered, setIsLabelHovered] = React.useState(false);
+
+  const handleLabelMouseEnter = () => {
+    setIsLabelHovered(true);
+  };
+
+  const handleLabelMouseLeave = () => {
+    setIsLabelHovered(false);
+  };
+
   return (
     <div
       style={{
@@ -27,11 +45,11 @@ export const CoolBundleCard = ({
     >
       <div
         style={{
+          mixBlendMode: "hard-light",
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
           backdropFilter: "blur(3px)",
-          mixBlendMode: "hard-light",
           width: "100%",
           height: "100%",
           overflow: "hidden",
@@ -41,29 +59,59 @@ export const CoolBundleCard = ({
         {children}
       </div>
       <div
+        onMouseEnter={handleLabelMouseEnter}
+        onMouseLeave={handleLabelMouseLeave}
+        onClick={
+          onLabelClick !== undefined && !labelMuted ? onLabelClick : () => {}
+        }
         style={{
+          cursor: `${label === "" ? "default" : isLabelHovered && !labelMuted ? "pointer" : "default"}`,
+          mixBlendMode: `${label === "" ? "hard-light" : isLabelHovered && !labelMuted ? "plus-lighter" : "normal"}`,
           backdropFilter: "blur(4px)",
           width: "100%",
           color: "white",
-          backgroundColor: "#00000033",
+          background: `${
+            labelBackground === undefined
+              ? label === ""
+                ? "#00000033"
+                : `linear-gradient(0.25turn, rgba(255, 104, 240, ${labelMuted ? "0.8" : "1"}), rgb(91, 196, 255, ${labelMuted ? "0.8" : "1"}))`
+              : labelBackground
+          }`,
           borderRadius: `0 0 ${radius} ${radius}`,
         }}
       >
         <span
           style={{
-            marginInlineStart: "10px",
+            display: "inline-block",
             position: "relative",
+            width: "100%",
             bottom: "0.65em",
             fontWeight: "bold",
+            height: `${label === "" ? "1.5em" : "2.6em"}`,
             fontFamily: "Inika, sans-serif",
             fontSize: "1.5em",
             textShadow: "0 0 10px #00000088",
-            paddingInlineStart: "10px",
           }}
         >
           <span>{title}</span>
-          <span style={{ paddingInlineStart: "8px", fontSize: "66%" }}>
+          <span style={{ paddingInlineStart: "28px", fontSize: "66%" }}>
             {subtitle}
+          </span>
+          <span
+            style={{
+              position: "absolute",
+              display: "block",
+              paddingBlock: `${label !== "" ? "0.8em" : "0"}`,
+              bottom: "-1.1em",
+              color: "white",
+              left: "50%",
+              transform: "translateX(-50%)",
+              textShadow: "none",
+              fontSize: "1.8em",
+              mixBlendMode: `${labelMuted ? "soft-light" : "normal"}`,
+            }}
+          >
+            {label}
           </span>
         </span>
       </div>
